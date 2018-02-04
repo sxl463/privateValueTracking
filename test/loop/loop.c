68b668ce20f3a2200d4191c70db7fea49cb80c73
sxl463 <sxl463@gmail.com>
2018-02-03 22:11:17 -0500

    loop

---
+++ test/loop/loop.c
@@ -0,0 +1,31 @@
+#include "CacheStateMagic.h"
+
+int example ()
+{
+  int PRIV sec; 
+  int a,b,c,d,e,f,g;
+ 
+
+ 
+      while (f > 0)
+	{
+	  a++;
+	  b++;
+	  c++;
+	  d++;
+	  e++;
+	  if (sec == 0 )
+	  {
+		  break;
+	  }
+	  else
+	  {
+		  
+	  }
+	 
+	} 
+      
+   
+
+ return 1;
+}
